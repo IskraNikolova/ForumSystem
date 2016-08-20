ac82e09cdf1413a44323793812a0bd709cfe5c68
IskraNikolova <air_iskra@yahoo.com>
2016-08-20 11:11:20 +0300

    Fix errors(add references to ForumSystem.Web)

--- Source/Data/ForumSystem.Models/ApplicationUser.cs
+++ Source/Data/ForumSystem.Models/ApplicationUser.cs
@@ -1,11 +1,13 @@
 ﻿namespace ForumSystem.Models
 {
+    using System;
     using System.Threading.Tasks;
     using System.Security.Claims;
+    using Data.Common.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
 
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditInfo, IDeletableEntity
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -14,5 +16,15 @@
             // Add custom user claims here
             return userIdentity;
         }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
