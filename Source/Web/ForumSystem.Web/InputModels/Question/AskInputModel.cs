a0591744385a48da1b2b82d0b5fe1779c4343076
IskraNikolova <air_iskra@yahoo.com>
2016-08-21 11:09:45 +0300

    Added TinyMce package

--- Source/Web/ForumSystem.Web/InputModels/Question/AskInputModel.cs
+++ Source/Web/ForumSystem.Web/InputModels/Question/AskInputModel.cs
@@ -1,12 +1,14 @@
 ﻿namespace ForumSystem.Web.InputModels.Question
 {
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
 
     public class AskInputModel
     {
         [Display(Name = "Title")]
         public string Title { get; set; }
 
+        [AllowHtml]
         [Display(Name = "Content")]
         [DataType(DataType.MultilineText)]
         public string Content { get; set; }
