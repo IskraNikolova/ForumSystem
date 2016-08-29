fe8e4a9f78dbd86df7aed7132786c3c332aaba41
IskraNikolova <air_iskra@yahoo.com>
2016-08-29 13:40:19 +0300

    Add new view model for index page

--- Source/Web/ForumSystem.Web/Controllers/HomeController.cs
+++ Source/Web/ForumSystem.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Linq;
     using System.Web.Mvc;
@@ -11,19 +13,23 @@
     public class HomeController : Controller
     {
         private readonly IDeletableEntityRepository<Post> posts;
+        private readonly IDeletableEntityRepository<ApplicationUser> users;
 
-        public HomeController(IDeletableEntityRepository<Post> posts)
+        public HomeController(IDeletableEntityRepository<Post> posts, IDeletableEntityRepository<ApplicationUser> users)
         {
             this.posts = posts;
+            this.users = users;
         }
 
         public ActionResult Index()
         {
-            var allposts = this.posts.All()
+            var allPosts = this.posts.All()
                     .Project().To<IndexBlogPostViewModel>()
                     .ToList();
 
-            return this.View(allposts);
+            var allUsers = this.users.All().ToList();
+            var modelForIndexPage = new Tuple<IList<ApplicationUser>, IList<IndexBlogPostViewModel>>(allUsers, allPosts);
+            return this.View(modelForIndexPage);
         }
     }
 }
---
+++ Source/Web/ForumSystem.Web/Logic/GetUserWithTheBestResult.cs
@@ -0,0 +1,20 @@
+﻿namespace ForumSystem.Web.Logic
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using ForumSystem.Models;
+
+    public static class GetUserWithTheBestResult
+    {
+        private const int DefaultLengthForRating = 10;
+        public static IList<ApplicationUser> RatingByPoints(IList<ApplicationUser> allUser)
+        {
+            var result = allUser
+                .OrderByDescending(u => u.Points)
+                .Take(DefaultLengthForRating)
+                .ToList();
+
+            return result;
+        }
+    }
+}
